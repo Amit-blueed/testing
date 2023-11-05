c5c1c1047bc515d7f0c6d019a10346ab974a4a6d
Amit Anand <amitanand@blueed.com>
2023-11-05 11:02:46 +0530

    adding constant file

---
+++ src/main/java/com/mycompany/app/Config.java
@@ -0,0 +1,10 @@
+package com.mycompany.app;
+
+public class Config {
+
+	public static final String BROKER_URL = "tcp://localhost:61616";
+	public static final String DESTINATION_NAME = "table_sync";
+
+	public static final String DB_URL_1 = "jdbc:postgresql://localhost:5432/postgres";
+	public static final String DB_URL_2 = "jdbc:postgresql://localhost:5432/postgres";
+}
